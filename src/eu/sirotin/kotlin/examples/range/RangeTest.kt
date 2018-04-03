7157faae7e6381dfe5f40a835d42163eca193c7c
Viktor Sirotin <vxod@sirotin.eu>
2018-04-03 09:02:28 +0200

    Small extensions in range example.

--- src/eu/sirotin/kotlin/examples/range/RangeTest.kt
+++ src/eu/sirotin/kotlin/examples/range/RangeTest.kt
@@ -4,5 +4,6 @@ package eu.sirotin.kotlin.examples.range
  * @author vsirotin
  */
 fun main(args: Array<String>){
-    println("Java" in "ABBA".."Love")
+    println("Beatles" in "ABBA".."Who")
+    println("Kotlin" in "Go".."Yaml")
 }
