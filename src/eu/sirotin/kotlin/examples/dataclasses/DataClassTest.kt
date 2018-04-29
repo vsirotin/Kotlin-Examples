dede545dcb82e9ef6a9083ba635e7bf71f27eefe
vsirotin <vxod@sirotin.eu>
2018-04-29 11:41:09 +0200

    data classes copy example

--- src/eu/sirotin/kotlin/examples/dataclasses/DataClassTest.kt
+++ src/eu/sirotin/kotlin/examples/dataclasses/DataClassTest.kt
@@ -5,9 +5,15 @@ fun main(args : Array<String>){
     val agent2 = Agent("Bond", "007")
     agent2.age = 50
     println("agent1=$agent1")
-    println("agent2=$agent2")
+    println("agent2=$agent2, age=${agent2.age}")
     println("== : ${agent1 == agent2}")
     println("=== : ${agent1 === agent2}")
+
+    val agent3 = agent2.copy()
+    println("agent3=$agent3, age=${agent3.age}")
+
+    val agent4 = agent2.copy(id="008")
+    println("agent4=$agent4")
 }
 
 data class Agent(val name: String, val id: String) {
