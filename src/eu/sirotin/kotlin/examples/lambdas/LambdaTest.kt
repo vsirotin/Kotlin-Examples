81aff37888fd980ea263efbb47774bf5b9f7defa
Viktor Sirotin <vxod@sirotin.eu>
2018-05-08 09:17:00 +0200

    Lambda. Step 2

--- src/eu/sirotin/kotlin/examples/lambdas/LambdaTest.kt
+++ src/eu/sirotin/kotlin/examples/lambdas/LambdaTest.kt
@@ -6,6 +6,14 @@ fun main(args : Array<String>) {
     println(people.sortedBy{it.name})
     println(people.minBy { it.age})
     println(people.maxBy(Person::age))
+    println(people.maxBy({p: Person ->p.age}))
+    run{println("Aha!")}
+
+    val names = people.joinToString (separator = ", ", transform =  {p: Person -> p.name} )
+    println(names)
+
+    val names1 = people.joinToString("|") {p: Person -> p.name}
+    println(names1)
 }
 
 data class Person(val name: String, val age: Int)
