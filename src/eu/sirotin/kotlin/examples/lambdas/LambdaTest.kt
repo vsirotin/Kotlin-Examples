4d9aad441b8f1a979eee8b0e62aeadb5d4c7856d
Viktor Sirotin <vxod@sirotin.eu>
2018-05-11 09:04:36 +0200

    3

--- src/eu/sirotin/kotlin/examples/lambdas/LambdaTest.kt
+++ src/eu/sirotin/kotlin/examples/lambdas/LambdaTest.kt
@@ -7,13 +7,29 @@ fun main(args : Array<String>) {
     println(people.minBy { it.age})
     println(people.maxBy(Person::age))
     println(people.maxBy({p: Person ->p.age}))
+    println(people.maxBy({it.age}))
     run{println("Aha!")}
+    println(sum(2, 4))
 
     val names = people.joinToString (separator = ", ", transform =  {p: Person -> p.name} )
     println(names)
 
     val names1 = people.joinToString("|") {p: Person -> p.name}
     println(names1)
+    println(z)
+    println(adder)
+    println(z)
+    z =20
+    println(adder)
+    println(z)
 }
 
 data class Person(val name: String, val age: Int)
+
+val sum ={x : Int, y : Int ->
+    print("$x + $y = ")
+    x + y
+}
+
+var z : Int = 12
+val adder: () -> Int = {z + 3 }
