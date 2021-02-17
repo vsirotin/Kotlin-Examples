4002c3f47b685dd703cb78e23f9f4f2f754098ec
viktorsirotin <vxod@sirotin.eu>
2021-02-17 13:49:50 +0100

    Matrix class extended

--- src/eu/sirotin/kotlin/examples/matrix/Matrix.kt
+++ src/eu/sirotin/kotlin/examples/matrix/Matrix.kt
@@ -1,8 +1,25 @@
 package eu.sirotin.kotlin.examples.matrix
 
+fun <T> matrixOf(vararg rows: Row<T>): Matrix<T>{
+    if(rows.isEmpty())throw IllegalArgumentException("No rows set")
+
+    val colNumber = rows[0].data.size
+    if(colNumber == 0)throw IllegalArgumentException("First row is empty")
+
+    if(rows.any { it.data.size != colNumber }) throw IllegalArgumentException("Rows have different length")
+
+    val result = Matrix<T>(rows.size, colNumber)
+    rows.forEachIndexed{indexRow, row->result.setRow(indexRow, row)}
+    return result
+}
+
+fun <T> rowOf(vararg elements: T): Row<T>{
+    return Row(*elements)
+}
+
 class Matrix<T>(private val rowNumber: Int, private val columnNumber: Int) {
     private var data : MutableMap<Int, Map<Int, T>>? = null
-    fun set(rowPos: Int, colPos: Int, value: T) {
+    operator fun set(rowPos: Int, colPos: Int, value: T) {
         if((rowPos < 0) || (rowPos >= rowNumber))throw IllegalArgumentException("False value rowPos")
         if((colPos < 0) || (colPos >= columnNumber))throw IllegalArgumentException("False value colPos")
         if(data == null)data = HashMap()
@@ -14,7 +31,7 @@ class Matrix<T>(private val rowNumber: Int, private val columnNumber: Int) {
         data!![rowPos] = row
     }
 
-    fun get(rowPos: Int, colPos: Int): T?{
+    operator fun get(rowPos: Int, colPos: Int): T?{
         if(data == null)return null
         return when(val row = data!![rowPos]){
             null -> null
@@ -22,4 +39,12 @@ class Matrix<T>(private val rowNumber: Int, private val columnNumber: Int) {
         }
 
     }
+
+    fun setRow(indexRow: Int, row: Row<T>) {
+        row.data.forEachIndexed{indexColumn, value->this[indexRow, indexColumn] = value}
+    }
+}
+
+class Row<T>(vararg elements: T){
+    var data: Array<out T> = elements
 }
--- src/eu/sirotin/kotlin/examples/matrix/MatrixTest.kt
+++ src/eu/sirotin/kotlin/examples/matrix/MatrixTest.kt
@@ -3,9 +3,23 @@ package eu.sirotin.kotlin.examples.matrix
 
 fun main() {
     val m1 = Matrix<Double>(20,20)
-    var v1 = m1.get(10,11)
-    println("v1=$v1")
-    m1.set(10,11, 23.1)
-    v1 = m1.get(10,11)
-    println("v1=$v1")
+    var v1 = m1[10,11]
+    println("v1=$v1") //null
+    m1[10,11] = 23.1
+    v1 = m1[10,11]
+    println("v1=$v1") //23.1
+
+    val m2 = matrixOf<Int>(
+        rowOf(1, 2, 3),
+        rowOf(4,5,6)
+    )
+
+    var v2 = m2[0,2]
+    println("v2=$v2") //3
+
+    m2[0,2] = 10
+    v2 = m2[0,2]
+    println("v2=$v2") //10
+
+
 }
